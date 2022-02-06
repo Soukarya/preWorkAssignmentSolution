153fffebdeefc49f597dea988f10be787636181e
Soukarya <37843094+Soukarya@users.noreply.github.com>
2022-02-06 12:49:28 +0530

    minor modifications

--- GreatLearning/ACP-FSD/Prework/UngradedAssignment/src/Main.java
+++ GreatLearning/ACP-FSD/Prework/UngradedAssignment/src/Main.java
@@ -11,30 +11,31 @@ public class Main {
 		System.out.print("Enter a number:");
 		int n = sc.nextInt();
 		int temp = n;
-		int m=0;
-		if(n>=1 && n<=9) {
+		int m = 0;
+		if (n < 0) {
+			System.out.println("Negative numbers are not palindromic");
+		} else if (n >= 0 && n <= 9) {
 			System.out.println("Palindrome number");
-		}else if(n>=10 && n<=99) {
-			if(n%11==0) {
+		} else if (n >= 10 && n <= 99) {
+			if (n % 11 == 0) {
 				System.out.println("Palindrome number");
-			}else {
+			} else {
 				System.out.println("Not a palindrome number");
 			}
-		}
-		else {
-			while(n>0) {
-				int d = n%10;
-				n/=10;
-				m=m*10+d;
+		} else {
+			while (n > 0) {
+				int d = n % 10;
+				n /= 10;
+				m = m * 10 + d;
 			}
-		
-			if(temp == m) {
+
+			if (temp == m) {
 				System.out.println("Palindrome number");
-			}else {
+			} else {
 				System.out.println("Not a Palindrome number");
 			}
 		}
-		
+
 	}
 
 	// function to printPattern
@@ -43,11 +44,16 @@ public class Main {
 
 		System.out.print("Enter a number:");
 		int n = sc.nextInt();
-		if(n==1) {
+		if (n <= 0) {
+			System.out.println("Please enter a valid number which is greater than 0");
+		} else if (n == 1) {
+			System.out.println("*");
+		} else if(n==2) {
+			System.out.println("**");
 			System.out.println("*");
 		}else {
-			for(int i=n;i>=1;i--) {
-				for(int j=1;j<=i;j++) {
+			for (int i = n; i >= 1; i--) {
+				for (int j = 1; j <= i; j++) {
 					System.out.print("*");
 				}
 				System.out.println();
@@ -61,26 +67,24 @@ public class Main {
 
 		System.out.print("Enter a number:");
 		int n = sc.nextInt();
-		boolean isPrime=true;
-		if(n==1) {
-			isPrime=false;
-		}
-		else if(n==2 || n==3) {
-			isPrime=true;
-		}
-		else if(n%2==0 || n%3==0){
-			isPrime=false;
-		}else {
-			for(int i=5;i*i<=n;i=i+6) {
-				if(n%i==0 || n%(i+2)==0) {
-					isPrime=false;
+		boolean isPrime = true;
+		if (n == 1) {
+			isPrime = false;
+		} else if (n == 2 || n == 3) {
+			isPrime = true;
+		} else if (n % 2 == 0 || n % 3 == 0) {
+			isPrime = false;
+		} else {
+			for (int i = 5; i * i <= n; i = i + 6) {
+				if (n % i == 0 || n % (i + 2) == 0) {
+					isPrime = false;
 					break;
 				}
 			}
 		}
-		if(!isPrime) {
+		if (!isPrime) {
 			System.out.println("Not a prime number");
-		}else {
+		} else {
 			System.out.println("Prime number");
 		}
 	}
@@ -89,15 +93,14 @@ public class Main {
 
 	public void printFibonacciSeries() {
 
-		// initialize the first and second value as 0,1 respectively.
 		System.out.print("Enter a number:");
 		int n = sc.nextInt();
 		int first = -1, second = 1;
-		for(int i=1;i<=n;i++) {
-			int temp=first+second;
-			System.out.print(temp+",");
-			first=second;
-			second=temp;
+		for (int i = 1; i <= n; i++) {
+			int temp = first + second;
+			System.out.print(temp + ",");
+			first = second;
+			second = temp;
 		}
 
 	}
